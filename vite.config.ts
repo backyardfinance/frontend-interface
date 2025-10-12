5981daabc41a2bedeb0de0908f923d6446a95df9
voenkomatiwe <radionpon21@gmail.com>
2025-10-12 12:04:41 +0300

    upd v

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsPaths()],
+  plugins: [react(), tsPaths(), tailwindcss()],
   build: {
     cssMinify: "lightningcss",
     rollupOptions: {
@@ -16,5 +18,10 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ["react/jsx-runtime"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
