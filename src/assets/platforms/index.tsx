1434147f358bd8ffde97ffa2b454f1c4a1595819
Sonce <oleksndrkorn@gmail.com>
2025-10-13 18:49:32 +0300

    add strategy placeholder

--- src/assets/platforms/index.tsx
+++ src/assets/platforms/index.tsx
@@ -1,17 +1,18 @@
+import { cn } from "@/utils";
 import DriftIcon from "./Drift.svg?react";
 import HyloIcon from "./Hylo.svg?react";
 import JupyterIcon from "./Jupyter.svg?react";
 import SynatraIcon from "./Synatra.svg?react";
 
-export const getPlatformImage = (name: string) => {
+export const getPlatformImage = (name: string, className?: string) => {
   switch (name) {
     case "Drift":
-      return <DriftIcon className="h-full w-full" />;
+      return <DriftIcon className={cn(className)} />;
     case "Hylo":
-      return <HyloIcon className="h-full w-full" />;
+      return <HyloIcon className={cn(className)} />;
     case "Jupyter":
-      return <JupyterIcon className="h-full w-full" />;
+      return <JupyterIcon className={cn(className)} />;
     case "Synatra":
-      return <SynatraIcon className="h-full w-full" />;
+      return <SynatraIcon className={cn(className)} />;
   }
 };
