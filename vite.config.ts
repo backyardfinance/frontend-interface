bbbfa0342b664952bf2642f5887fa1a90cd05ed1
Sonce <oleksndrkorn@gmail.com>
2025-10-18 13:43:03 +0300

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
 import tsPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsPaths(), tailwindcss(), svgr()],
+  plugins: [react(), tsPaths(), tailwindcss(), svgr(), nodePolyfills()],
   build: {
     cssMinify: "lightningcss",
     rollupOptions: {
