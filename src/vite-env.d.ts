d86b5400d931658cc5335004a63d921b70d25804
voenkomatiwe <radionpon21@gmail.com>
2025-10-21 15:05:29 +0300

    feat: added open-api-tools

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PUBLIC_BACKEND_URL: string;
+}
+
+export interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
