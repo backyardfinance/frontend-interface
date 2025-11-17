b43e8603c221868f46a6e242443a77461b329e86
Sonce <oleksndrkorn@gmail.com>
2025-11-17 12:51:55 +0200

    return logic for redirect + update mainnet for staging

--- src/providers/solana-provider.tsx
+++ src/providers/solana-provider.tsx
@@ -6,9 +6,9 @@ import { clusterApiUrl } from "@solana/web3.js";
 import { type FC, type PropsWithChildren, useMemo } from "react";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { isDev } from "@/utils";
+// import { isDev } from "@/utils";
 
-const network = isDev() ? WalletAdapterNetwork.Devnet : WalletAdapterNetwork.Mainnet;
+const network =  WalletAdapterNetwork.Mainnet;
 
 const SolanaWalletProvider: FC<PropsWithChildren> = ({ children }) => {
   const endpoint = useMemo(() => clusterApiUrl(network), []);
