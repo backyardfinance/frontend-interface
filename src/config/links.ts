c13766d828228419f7865ba48a9f1c34d090b04d
Arsen Petrosian <xckaytz@gmail.com>
2025-11-17 17:14:46 +0200

    updated X post

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,4 +1,4 @@
 export const links = {
   x: "https://x.com/backyard_fi",
-  x_post: "https://x.com/backyard_fi/status/1980570148739465536",
+  x_post: "https://x.com/backyard_fi/status/1990438379855458808",
 };
