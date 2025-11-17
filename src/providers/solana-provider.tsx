db1e33f4d00962f77fae6df553bce22e7ff5e66b
Sonce <oleksndrkorn@gmail.com>
2025-11-17 12:59:02 +0200

    update provider

--- src/providers/solana-provider.tsx
+++ src/providers/solana-provider.tsx
@@ -1,17 +1,18 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
+// import { clusterApiUrl } from "@solana/web3.js";
 import { type FC, type PropsWithChildren, useMemo } from "react";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 // import { isDev } from "@/utils";
 
-const network =  WalletAdapterNetwork.Mainnet;
+// const network =  WalletAdapterNetwork.Mainnet;
 
 const SolanaWalletProvider: FC<PropsWithChildren> = ({ children }) => {
-  const endpoint = useMemo(() => clusterApiUrl(network), []);
+  const endpoint = 'https://mainnet.helius-rpc.com/?api-key=9394af7d-62ba-4dcd-a783-3d35c501e7d2';
+
   const wallets = useMemo(() => [new SolflareWalletAdapter(), new PhantomWalletAdapter()], []);
 
   return (
