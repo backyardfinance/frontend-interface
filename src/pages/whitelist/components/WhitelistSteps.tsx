8f412a3dc7fc7530c08fc1b5a25f4f95b16a0e0e
voenkomatiwe <radionpon21@gmail.com>
2025-11-18 21:40:12 +0200

    fix: whitelist steps

--- src/pages/whitelist/components/WhitelistSteps.tsx
+++ src/pages/whitelist/components/WhitelistSteps.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useSolanaWallet } from "@/hooks/useSolanaWallet";
 import { useIsMintedNFT } from "@/hooks/useWhitelistNFT";
 import { truncateAddress } from "@/utils";
@@ -11,9 +12,24 @@ import { ErrorMessage, InfoMessage } from "./ui";
 
 export const WhitelistSteps = () => {
   const { address } = useSolanaWallet();
-
   const { tasks, progress, xConnected, user } = useWhitelistUser();
   const { data: isMintedNFT } = useIsMintedNFT(address ?? "");
+
+  const { isSignWalletCompleted, isEmailCompleted, isFollowXCompleted, isQuotePostCompleted } = useMemo(() => {
+    const sign = tasks.walletConnected;
+    const email = sign && tasks.emailVerified;
+    const follow = email && tasks.xFollowed;
+    const quote = follow && tasks.postRetweeted;
+
+    return {
+      isSignWalletCompleted: sign,
+      isEmailCompleted: email,
+      isFollowXCompleted: follow,
+      isQuotePostCompleted: quote,
+    };
+  }, [tasks]);
+
+  const walletMismatch = address !== user?.wallet && user?.wallet;
 
   return (
     <div className="flex flex-col gap-15">
@@ -34,25 +50,26 @@ export const WhitelistSteps = () => {
         )}
       </div>
       <div className="flex flex-col items-start gap-5">
-        <SignWallet connectedAddress={user?.wallet} isCompleted={tasks.walletConnected} />
-        <Email connectedEmail={user?.email} disabled={!tasks.walletConnected} isCompleted={tasks.emailVerified} />
+        <SignWallet connectedAddress={user?.wallet} isCompleted={isSignWalletCompleted} />
+        <Email connectedEmail={user?.email} disabled={!isSignWalletCompleted} isCompleted={isEmailCompleted} />
         <FollowX
           connectedX={user?.twitterUsername}
-          disabled={!tasks.walletConnected}
-          isCompleted={tasks.xFollowed}
+          disabled={!isEmailCompleted}
+          isCompleted={isFollowXCompleted}
           xConnected={xConnected}
         />
-        <QuotePost disabled={!tasks.walletConnected || !tasks.xFollowed} isCompleted={tasks.postRetweeted} />
+        <QuotePost disabled={!isFollowXCompleted} isCompleted={isQuotePostCompleted} />
         <MintNFT connectedAddress={user?.wallet} disabled={!progress.isComplete} />
-        {address !== user?.wallet && user?.wallet && (
+
+        {walletMismatch && (
           <ErrorMessage
             message={`Wallet verification failed. Please connect the correct wallet: ${truncateAddress(user.wallet)}`}
           />
         )}
 
-        <p className="select-none font-bold text-[#E3D0FF] text-s leading-[128%]">
-          *If you're facing any issues, DM us on X:{" "}
-          <a href={"https://x.com/backyard_fi"} rel="noopener" target="_blank">
+        <p className="flex select-none items-center gap-1 font-bold text-[#E3D0FF] text-s leading-[128%]">
+          *If you're facing any issues, DM us on X:
+          <a href="https://x.com/backyard_fi" rel="noopener" target="_blank">
             @backyard_fi
           </a>
         </p>
