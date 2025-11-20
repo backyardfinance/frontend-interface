920612c6d2b34d3febd83abdbe372cca46b1fc22
Sonce <oleksndrkorn@gmail.com>
2025-11-20 20:31:01 +0200

    update logout check

--- src/pages/whitelist/index.tsx
+++ src/pages/whitelist/index.tsx
@@ -1,17 +1,12 @@
 import WhitelistBg from "@/assets/landing/whitelist-bg.webp";
 import { LandingHeader } from "@/components/header";
-import { useSolanaWallet } from "@/hooks/useSolanaWallet";
-import { useIsMintedNFT } from "@/hooks/useWhitelistNFT";
-import { localStorageService } from "@/services/localStorageService";
 import { WhitelistStats } from "./components/WhitelistStats";
 import { WhitelistSteps } from "./components/WhitelistSteps";
 import { WhitelistSuccess } from "./components/WhitelistSuccess";
+import { useWhitelistUser } from "./hooks/useWhitelistUser";
 
 export default function WhitelistPage() {
-  const { address } = useSolanaWallet();
-  const { data: isMintedNFT } = useIsMintedNFT(address ?? "");
-  const accessToken = localStorageService.getAccessToken();
-  const showWhitelistSuccess = accessToken && isMintedNFT;
+  const { progress } = useWhitelistUser();
 
   return (
     <div className="relative z-0 flex min-h-screen flex-col gap-12 px-4 pb-20 md:gap-24">
@@ -25,7 +20,7 @@ export default function WhitelistPage() {
 
       <div className="mx-auto flex w-full max-w-[1350px] flex-col-reverse gap-14 md:gap-8 lg:flex-row lg:justify-between">
         <div className="w-full lg:max-w-[840px]">
-          {showWhitelistSuccess ? <WhitelistSuccess /> : <WhitelistSteps />}
+          {progress?.isComplete ? <WhitelistSuccess /> : <WhitelistSteps />}
         </div>
         <WhitelistStats />
       </div>
